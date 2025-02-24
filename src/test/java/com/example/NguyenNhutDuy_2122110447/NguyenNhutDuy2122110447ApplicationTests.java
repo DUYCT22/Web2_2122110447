b73eb3e439a32385f1ce298dd025ef26fc227e2e
DUYCT22 <nhutduy11bkg@gmail.com>
2025-02-24 10:25:24 +0700

    newjava

---
+++ src/test/java/com/example/NguyenNhutDuy_2122110447/NguyenNhutDuy2122110447ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NguyenNhutDuy_2122110447;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenNhutDuy2122110447ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
